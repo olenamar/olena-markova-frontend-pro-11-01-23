01d2b3f1a30dbeed9dc8cc5b517235ae8f33b549
Olena <7890572@ukr.net>
2023-01-11 17:08:01 +0100

    Added a file

--- object.js
+++ object.js
@@ -33,22 +33,25 @@ const peoples = [
   }
 ];
 
-  function changes(user) {
-      for (let i = 0; i < peoples.length; i++){
-          const newPeoples = peoples[i];
-          newPeoples.name = newPeoples.name.toUpperCase();
-          newPeoples.cities = newPeoples.cities.split(',');
-              for(let j = 0; j < newPeoples.children.length; j++){
-                  const children = newPeoples.children[j];
-                  children.name = children.name.toUpperCase();
-                  
-                  }
-                  
-                  
-              }
-        return user;      
-      }
-           
-  
-  
+function changes(user) {
+    for (let i = 0; i < peoples.length; i++){
+        const newPeoples = peoples[i];
+        newPeoples.name = newPeoples.name.toUpperCase();
+        newPeoples.cities = newPeoples.cities.split(',');
+        newPeoples.money = Number(newPeoples.money);
+        if(!isNaN(newPeoples.money)){
+            newPeoples.money = Math.round(newPeoples.money); 
+        } else {
+            newPeoples.money = 'Не є число';
+        }
+        if(Array.isArray(newPeoples.children) && newPeoples.children.length || !newPeoples.children.length){
+            newPeoples.children.push({name: 'Stepan', age: Math.round(Math.random() * 18)})
+        }
+            for(let j = 0; j < newPeoples.children.length; j++){
+                const children = newPeoples.children[j];
+                children.name = children.name.toUpperCase();
+            }            
+    }
+    return user;
+}        
 console.log(changes(peoples));
